6bf9bf7896446c9473752123234242c9ebc19c4e
rodolfo-22 <100951955+rodolfo-22@users.noreply.github.com>
2024-07-07 01:48:24 -0600

    :hammer: add firts chabge to invitate

--- src/pages/visitant/Invitaciones.jsx
+++ src/pages/visitant/Invitaciones.jsx
@@ -1,93 +1,115 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getInvitations } from '../../services/Invitado/invitadoService';
 
 const Invitaciones = () => {
   const navigate = useNavigate();
+  const [invitations, setInvitations] = useState([]);
+
+  useEffect(() => {
+    const fetchInvitations = async () => {
+      try {
+        const data = await getInvitations();
+        const updatedData = data.map(invitation => {
+          let anfitrion = "N/A";
+          if (invitation.home && invitation.home.users) {
+            const residentEncargado = invitation.home.users.find(user => 
+              user.roles && user.roles.includes("RSDT")
+            );
+            if (residentEncargado) {
+              anfitrion = residentEncargado.username;
+            }
+          }
+          return {
+            ...invitation,
+            anfitrion
+          };
+        });
+        setInvitations(updatedData);
+      } catch (error) {
+        console.error('Error fetching invitations:', error);
+      }
+    };
+
+    fetchInvitations();
+  }, []);
 
   const handleButtonClick = (path) => {
     navigate(path); // Navega a la ruta proporcionada
   };
 
-  const [invitations, setInvitations] = useState([]);
-
-  // Función simulada para obtener datos desde una base de datos
-  useEffect(() => {
-    // Reemplaza esto con tu lógica de obtención de datos
-    const fetchData = async () => {
-      const data = await new Promise((resolve) =>
-        setTimeout(() =>
-          resolve([
-            { anfitrion: 'Tony Stark', fecha: '25/05/2024', hora: '7:00 am' },
-            { anfitrion: 'Peter Parker', fecha: '26/05/2024', hora: '8:00 am' },
-            { anfitrion: 'Natasha Romanoff', fecha: '27/05/2024', hora: '9:00 am' },
-            { anfitrion: 'Steve Rogers', fecha: '27/05/2024', hora: '9:00 am' },
-            { anfitrion: 'Thor Odinson', fecha: '27/05/2024', hora: '9:00 am' },
-            { anfitrion: 'Nick Fury', fecha: '27/05/2024', hora: '9:00 am' }
-
-          ]), 1000)
-      );
-      setInvitations(data);
-    };
-
-    fetchData();
-  }, []);
+  const isSingleDayInvitation = (dates) => {
+    if (dates.length === 1) {
+      const start = new Date(dates[0].start_datetime);
+      const end = new Date(dates[0].end_datetime);
+      return start.toDateString() === end.toDateString();
+    }
+    return false;
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h1 className="mb-6 text-3xl font-bold font-roboto_mono text-azul-claro">Mis invitaciones</h1>
       <div className="w-full max-w-4xl h-96 overflow-y-auto md:h-auto md:overflow-y-visible">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {invitations.map((invitation, index) => (
-            <div key={index} className="w-full max-w-md p-6 bg-white shadow-md rounded-lg">
-              <div className="bg-white shadow-md rounded-lg p-4 border">
-                <div className="mb-4">
-                  <label className="block text-black text-sm font-bold font-roboto_mono mb-2" htmlFor={`anfitrion-${index}`}>
-                    Anfitrión:
-                  </label>
-                  <input
-                    className="font-roboto_mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id={`anfitrion-${index}`}
-                    type="text"
-                    value={invitation.anfitrion}
-                    readOnly
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-black text-sm font-roboto_mono font-bold mb-2" htmlFor={`fecha-${index}`}>
-                    Fecha programada:
-                  </label>
-                  <input
-                    className="font-roboto_mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id={`fecha-${index}`}
-                    type="text"
-                    value={invitation.fecha}
-                    readOnly
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-black text-sm font-roboto_mono font-bold mb-2" htmlFor={`hora-${index}`}>
-                    Hora:
-                  </label>
-                  <input
-                    className="font-roboto_mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id={`hora-${index}`}
-                    type="text"
-                    value={invitation.hora}
-                    readOnly
-                  />
-                </div>
-                <div className="flex items-center justify-center">
-                  <button
-                    className="bg-amarillo-principal hover:bg-yellow-600 text-white font-roboto_mono font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="button"
-                    onClick={() => handleButtonClick('/visits/qr')}
-                  >
-                    Validar
-                  </button>
+          {invitations.map((invitation, index) => {
+            const isSingleDay = isSingleDayInvitation(invitation.dates);
+            return (
+              <div key={index} className={`w-full max-w-md p-6 ${isSingleDay ? 'bg-white' : 'bg-yellow-100'} shadow-md rounded-lg`}>
+                <div className="bg-white shadow-md rounded-lg p-4 border">
+                  <div className="mb-4">
+                    <label className="block text-black text-sm font-bold font-roboto_mono mb-2" htmlFor={`anfitrion-${index}`}>
+                      Anfitrión:
+                    </label>
+                    <input
+                      className="font-roboto_mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id={`anfitrion-${index}`}
+                      type="text"
+                      value={invitation.anfitrion || 'N/A'}
+                      readOnly
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-black text-sm font-roboto_mono font-bold mb-2" htmlFor={`fecha-${index}`}>
+                      Fecha programada:
+                    </label>
+                    <input
+                      className="font-roboto_mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id={`fecha-${index}`}
+                      type="text"
+                      value={isSingleDay
+                        ? (invitation.dates[0].start_datetime.split('T')[0] || 'N/A')
+                        : `${invitation.dates[0].start_datetime.split('T')[0]} a ${invitation.dates[invitation.dates.length - 1].end_datetime.split('T')[0]}`}
+                      readOnly
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-black text-sm font-roboto_mono font-bold mb-2" htmlFor={`hora-${index}`}>
+                      Hora:
+                    </label>
+                    <input
+                      className="font-roboto_mono shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id={`hora-${index}`}
+                      type="text"
+                      value={isSingleDay
+                        ? (invitation.dates[0].start_datetime.split('T')[1] || 'N/A')
+                        : `${invitation.dates[0].start_datetime.split('T')[1]} a ${invitation.dates[invitation.dates.length - 1].end_datetime.split('T')[1]}`}
+                      readOnly
+                    />
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <button
+                      className="bg-amarillo-principal hover:bg-yellow-600 text-white font-roboto_mono font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="button"
+                      onClick={() => handleButtonClick('/visits/qr')}
+                    >
+                      Validar
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
@@ -95,3 +117,4 @@ const Invitaciones = () => {
 };
 
 export default Invitaciones;
+

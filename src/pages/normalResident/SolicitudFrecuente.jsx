6c49aab58bd0cf8704b105c5be91d57907c896a3
rodolfo-22 <100951955+rodolfo-22@users.noreply.github.com>
2024-07-06 19:10:28 -0600

    :hammer: normal residen connected

--- src/pages/normalResident/SolicitudFrecuente.jsx
+++ src/pages/normalResident/SolicitudFrecuente.jsx
@@ -1,100 +1,115 @@
 import { useState } from "react";
 import { usePopup } from "../../components/PopupContext";
+import { sendMultipleInvitation } from "../../services/ResidenteNormal/invitationService";
 
 const SolicitudFrecuente = () => {
+    const { showPopup } = usePopup();
 
-  const { showPopup } = usePopup();
+    const [formValues, setFormValues] = useState({
+        email: '',
+        date1: '',
+        date2: '',
+        time1: '',
+        time2: ''
+    });
 
-  const [formValues, setFormValues] = useState({
-      email: '',
-      dateVisit: '',
-      timeVisit: ''
-  });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        });
+    };
 
-  const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormValues({
-          ...formValues,
-          [name]: value
-      });
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const houseNumber = localStorage.getItem('houseNumber');
+        const requestData = {
+            house_number: houseNumber,
+            user_identifier: formValues.email,
+            request: true, // Para que se verifique por el encargo del hogar
+            initial_dates: [`${formValues.date1}T${formValues.time1}:00`],
+            final_dates: [`${formValues.date2}T${formValues.time2}:00`]
+        };
 
-  const handleSubmit = (e) => {
-      e.preventDefault();
-      // Aquí puedes agregar la lógica para enviar el formulario a una API
-      console.log('Formulario enviado:', formValues);
-      showPopup("Solicitud enviada exitosamente", true);
-  };
-  const handleCancel = (e) => {
-      e.preventDefault();
-      // Aquí puedes agregar la lógica para cancelar el formulario
-      showPopup("Solicitud cancelado", false);
-  };
+        try {
+            const response = await sendMultipleInvitation(requestData);
+            console.log('Formulario enviado:', response);
+            showPopup("Solicitud enviada exitosamente", true);
+        } catch (error) {
+            console.error('Error al enviar el formulario:', error);
+            showPopup("Error al enviar la solicitud", false);
+        }
+    };
 
-  return (
-      <div className="flex  items-center h-screen flex-col  w-full  font-roboto_mono ">
+    const handleCancel = (e) => {
+        e.preventDefault();
+        showPopup("Solicitud cancelada", false);
+    };
 
-          <h1 className="text-2xl text-[#6185A9] text-center m-2 ">Detalles de la solicitud</h1>
-          <h2 className="text-center text-xl text-[#6185A9] pb-2">Acceso Frecuente</h2>
-          <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-              <div className="flex flex-col mt-4 mb-4">
-                  <label className="text-lg mb-2">Correo electrónico:</label>
-                  <input
-                      type="email"
-                      name="email"
-                      placeholder="Correo electrónico"
-                      className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      value={formValues.email}
-                      onChange={handleChange}
-                  />
-              </div>
-              <div className="flex flex-col mt-4 mb-4">
-                  <label className="text-lg mb-2">Fecha inicial de visita:</label>
-                  <input
-                      type="date"
-                      name="date1"
-                      className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      value={formValues.date}
-                      onChange={handleChange}
-                  />
-              </div>
-              <div className="flex flex-col mt-4 mb-4">
-                  <label className="text-lg mb-2">Fecha final de visita:</label>
-                  <input
-                      type="date"
-                      name="date2"
-                      className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      value={formValues.date}
-                      onChange={handleChange}
-                  />
-              </div>
-              <div className="flex flex-col mt-4">
-                  <label className="text-lg mb-2">Permiso habilitado desde:</label>
-                  <input
-                      type="time"
-                      name="time1"
-                      className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      value={formValues.time}
-                      onChange={handleChange}
-                  />
-              </div>
-              <div className="flex flex-col mt-4 mb-8">
-                  <label className="text-lg mb-2">Hasta:</label>
-                  <input
-                      type="time"
-                      name="time2"
-                      className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      value={formValues.time}
-                      onChange={handleChange}
-                  />
-              </div>
-              <div className="flex justify-between">
-                  <button type="submit" className="p-2 rounded-lg bg-[#F8BD0D] bg-opacity-70 w-full md:w-1/2 lg:w-1/3 mx-2">Aceptar</button>
-                  <button type="button" className="p-2 rounded-lg bg-[#F79E9E] w-full md:w-1/2 lg:w-1/3 mx-2" onClick={handleCancel} >Cancelar</button>
-              </div>
-          </form>
-      </div>
-  );
+    return (
+        <div className="flex items-center h-screen flex-col w-full font-roboto_mono">
+            <h1 className="text-2xl text-[#6185A9] text-center m-2">Detalles de la solicitud</h1>
+            <h2 className="text-center text-xl text-[#6185A9] pb-2">Acceso Frecuente</h2>
+            <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+                <div className="flex flex-col mt-4 mb-4">
+                    <label className="text-lg mb-2">Correo electrónico:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Correo electrónico"
+                        className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value={formValues.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col mt-4 mb-4">
+                    <label className="text-lg mb-2">Fecha inicial de visita:</label>
+                    <input
+                        type="date"
+                        name="date1"
+                        className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value={formValues.date1}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col mt-4 mb-4">
+                    <label className="text-lg mb-2">Fecha final de visita:</label>
+                    <input
+                        type="date"
+                        name="date2"
+                        className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value={formValues.date2}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col mt-4">
+                    <label className="text-lg mb-2">Permiso habilitado desde:</label>
+                    <input
+                        type="time"
+                        name="time1"
+                        className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value={formValues.time1}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex flex-col mt-4 mb-8">
+                    <label className="text-lg mb-2">Hasta:</label>
+                    <input
+                        type="time"
+                        name="time2"
+                        className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value={formValues.time2}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex justify-between">
+                    <button type="submit" className="p-2 rounded-lg bg-[#F8BD0D] bg-opacity-70 w-full md:w-1/2 lg:w-1/3 mx-2">Aceptar</button>
+                    <button type="button" className="p-2 rounded-lg bg-[#F79E9E] w-full md:w-1/2 lg:w-1/3 mx-2" onClick={handleCancel}>Cancelar</button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default SolicitudFrecuente;
--- src/pages/normalResident/SolicitudUnica.jsx
+++ src/pages/normalResident/SolicitudUnica.jsx
@@ -1,82 +1,96 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { usePopup } from "../../components/PopupContext";
+import { sendInvitation } from "../../services/ResidenteNormal/invitationService";
 
-const  SolicitudUnica = () => {
-  const { showPopup } = usePopup();
+const SolicitudUnica = () => {
+    const { showPopup } = usePopup();
 
     const [formValues, setFormValues] = useState({
         email: '',
-        dateVisit: '',
-        timeVisit: ''
-
-    })
+        date: '',
+        time: ''
+    });
 
     const handleChange = (e) => {
-        const { name, value} = e.target;
+        const { name, value } = e.target;
         setFormValues({
-            ...formValues,
-            [name]: value
+        ...formValues,
+        [name]: value
         });
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Aquí puedes agregar la lógica para enviar el formulario a una API
-        console.log('Formulario enviado:', formValues);
+        const houseNumber = localStorage.getItem('houseNumber');
+        const requestData = {
+        house_number: houseNumber,
+        user_identifier: formValues.email,
+        request: true,  // para que solicite revision por parte del encargado
+        initial_dates: [`${formValues.date}T${formValues.time}:00`],
+        final_dates: [`${formValues.date}T${formValues.time}:00`]
+        };
+
+        try {
+        const response = await sendInvitation(requestData);
+        console.log('Formulario enviado:', response);
         showPopup("Solicitud enviada exitosamente", true);
-    }
-    const handleCancel = (e) => {
-        e.preventDefault();
-        // Aquí puedes agregar la lógica para cancelar el formulario
-        showPopup("Solicitud cancelado", false);
+        } catch (error) {
+        console.error('Error al enviar el formulario:', error);
+        showPopup("Error al enviar la solicitud", false);
+        }
     };
 
-    return(
-        <div className="flex  items-center h-screen flex-col  w-full  font-roboto_mono ">
-            <h1 className="text-2xl text-[#6185A9] text-center pt-4 pb-1">Detalles de la solicitud</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="flex flex-col mt-4 mb-4 width-auto">
-                <label className="text-xl mb-2">Correo electrónico:</label>
-                <input 
-                    type="email" 
-                    name="email" 
-                    placeholder="Correo electrónico"
-                    className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    value={formValues.email} 
-                    onChange={handleChange} 
-                />
-                </div>
-                <div className=" flex flex-col mt-4 mb-4">
-                <label className="text-xl mb-2">Fecha de visita:</label>
-                <input 
-                    type="date" 
-                    name="date" 
-                    className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    value={formValues.date} 
-                    onChange={handleChange} 
-                />
-                </div>
-                <div className=" flex flex-col mt-4 mb-8">
-                <label className="text-xl mb-2">Hora de llegada:</label>
-                <input 
-                    type="time" 
-                    name="time" 
-                    className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    value={formValues.time} 
-                    onChange={handleChange} 
-                />
-                </div>
-                <div className="flex flex-row pt-2">
-                    <div className="p-4 mr-4 rounded-lg bg-[#F8BD0D] bg-opacity-70">
-                        <button type="submit">Aceptar</button>
-                    </div>
-                    <div className="p-4 rounded-lg bg-[#F79E9E]">
-                        <button type="button" onClick={handleCancel}>Cancelar</button>
-                    </div>
-                </div>
-            </form>
+    const handleCancel = (e) => {
+        e.preventDefault();
+        showPopup("Solicitud cancelada", false);
+    };
+
+    return (
+        <div className="flex items-center h-screen flex-col w-full font-roboto_mono">
+        <h1 className="text-2xl text-[#6185A9] text-center pt-4 pb-1">Detalles de la solicitud</h1>
+        <form onSubmit={handleSubmit}>
+            <div className="flex flex-col mt-4 mb-4 width-auto">
+            <label className="text-xl mb-2">Correo electrónico:</label>
+            <input 
+                type="email" 
+                name="email" 
+                placeholder="Correo electrónico"
+                className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={formValues.email} 
+                onChange={handleChange} 
+            />
+            </div>
+            <div className="flex flex-col mt-4 mb-4">
+            <label className="text-xl mb-2">Fecha de visita:</label>
+            <input 
+                type="date" 
+                name="date" 
+                className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={formValues.date} 
+                onChange={handleChange} 
+            />
+            </div>
+            <div className="flex flex-col mt-4 mb-8">
+            <label className="text-xl mb-2">Hora de llegada:</label>
+            <input 
+                type="time" 
+                name="time" 
+                className="border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={formValues.time} 
+                onChange={handleChange} 
+            />
+            </div>
+            <div className="flex flex-row pt-2">
+            <div className="p-4 mr-4 rounded-lg bg-[#F8BD0D] bg-opacity-70">
+                <button type="submit">Aceptar</button>
+            </div>
+            <div className="p-4 rounded-lg bg-[#F79E9E]">
+                <button type="button" onClick={handleCancel}>Cancelar</button>
+            </div>
+            </div>
+        </form>
         </div>
     );
-    }
-  
-  export default SolicitudUnica;
+}
+
+export default SolicitudUnica;
